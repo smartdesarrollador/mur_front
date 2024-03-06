7f1cca82df660cd0a32b6684bfdf4097a0775792
Jeans <sistemadesignstyle@gmail.com>
2024-03-06 12:21:20 -0500

    feature con borde

---
+++ src/app/layout/componentes/feature-2/feature-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './feature-2.component.html',
+  styleUrl: './feature-2.component.css'
+})
+export class Feature2Component {
+
+}
--- src/app/pages/vista/inicio/inicio.component.ts
+++ src/app/pages/vista/inicio/inicio.component.ts
@@ -5,6 +5,7 @@ import { Feature1Component } from 'src/app/layout/componentes/feature-1/feature-
 import { Services1Component } from 'src/app/layout/componentes/services-1/services-1.component';
 import { CarouselOwl2Component } from 'src/app/layout/componentes/carousel-owl-2/carousel-owl-2.component';
 import { SubtituloParrafo1Component } from 'src/app/layout/componentes/subtitulo-parrafo-1/subtitulo-parrafo-1.component';
+import { Feature2Component } from 'src/app/layout/componentes/feature-2/feature-2.component';
 
 @Component({
   selector: 'app-inicio',
@@ -16,6 +17,7 @@ import { SubtituloParrafo1Component } from 'src/app/layout/componentes/subtitulo
     Services1Component,
     CarouselOwl2Component,
     SubtituloParrafo1Component,
+    Feature2Component,
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
