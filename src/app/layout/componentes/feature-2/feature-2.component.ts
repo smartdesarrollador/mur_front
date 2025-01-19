705a3f52497afde4c1a4d3cafbeeacd54d494369
Jeans <sistemadesignstyle@gmail.com>
2025-01-19 00:42:19 -0500

    listando features

--- src/app/layout/componentes/feature-2/feature-2.component.ts
+++ src/app/layout/componentes/feature-2/feature-2.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { MisionService } from 'src/app/services/mision.service';
+import { Mision } from 'src/app/models/mision';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feature-2',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './feature-2.component.html',
   styleUrl: './feature-2.component.css',
 })
-export class Feature2Component {}
+export class Feature2Component implements OnInit {
+  datos_mision: any = [];
+
+  constructor(private misionService: MisionService) {}
+
+  ngOnInit(): void {
+    this.cargarMisiones();
+  }
+
+  cargarMisiones() {
+    this.misionService.getCategories().subscribe((data: any) => {
+      console.log(data);
+      this.datos_mision = data;
+    });
+  }
+}
